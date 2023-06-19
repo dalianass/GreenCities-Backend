e9684fb30e61f0bac701b919487d2b41b94984eb
Dalila Pramenkovic <pramenkovicdalila@gmail.com>
2023-06-19 16:09:38 +0200

    Some changes

--- build/generated/sources/annotationProcessor/java/main/org/i4di/green/dto/mapper/UserMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/org/i4di/green/dto/mapper/UserMapperImpl.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-06-19T15:28:11+0200",
+    date = "2023-06-19T15:39:07+0200",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_202 (Oracle Corporation)"
 )
 @Component
